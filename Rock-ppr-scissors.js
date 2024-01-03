877879e25c2ea9ddfab57cd1b41541b7b8e4dd70
Prabal-verma <122899996+Prabal-verma@users.noreply.github.com>
2024-01-04 03:30:20 +0530

    add eventlister to keypress

--- Rock-ppr-scissors.js
+++ Rock-ppr-scissors.js
@@ -48,6 +48,17 @@ document.querySelector(".js-scissors-button").addEventListener('click',()=>{
   playGame('scissors');
 })
 
+document.body.addEventListener('keydown',(event)=>{
+  if(event.key === 'r'){
+    playGame('rock');
+  }
+  else if(event.key === 'p'){
+    playGame('paper');
+  }
+  else if(event.key === 's'){
+    playGame('scissors');
+  }
+})
 
 
 function playGame(plrMove){
